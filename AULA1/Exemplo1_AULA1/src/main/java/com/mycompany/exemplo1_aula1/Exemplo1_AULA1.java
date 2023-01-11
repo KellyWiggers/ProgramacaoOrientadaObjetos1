78bc67c645158093c761b15b977ae249b65a6121
KellyWiggers <kellylaiswiggers@hotmail.com>
2023-01-11 13:44:18 -0300

    Exemplos aula 1
    Exemplos de código apresentados na AULA 1 de POO1

---
+++ AULA1/Exemplo1_AULA1/src/main/java/com/mycompany/exemplo1_aula1/Exemplo1_AULA1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplo1_aula1;
+import javax.swing.*;
+/**
+ *
+ * @author kelly
+ */
+public class Exemplo1_AULA1 {
+
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("Titulo da Janela");
+        janela.setBounds(50,100,400,150);
+        janela.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        janela.setVisible(true); // Exibe a janela } 
+    }
+}
---
+++ AULA1/Exemplo2_AULA1/src/main/java/com/mycompany/exemplo2_aula1/Exemplo2_AULA1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exemplo2_aula1;
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author kelly
+ */
+public class Exemplo2_AULA1 {
+
+    public static void main(String[] args) {
+        int i;
+        JFrame janela = new JFrame("Título da janela");
+        janela.setBounds(50, 100, 400, 150); //Seta posição e tamanho
+        janela.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        FlowLayout flow = new FlowLayout(); //Define o layout do container
+        Container caixa = janela.getContentPane(); // Define o tamanho
+        caixa.setLayout(flow); // Seta layout do container
+        for (i = 1; i <= 6; i++)
+            caixa.add(new JButton("Aperte " + i)); // Adiciona um botão
+        JLabel label = new JLabel("Exemplo de texto:"); 
+    caixa.add(label); 
+    JTextField campo = new JTextField(15); 
+caixa.add(campo); 
+janela.pack(); // Redimensiona a janela
+
+        
+        
+        janela.setVisible(true); // Exibe a janela
+    }
+}
